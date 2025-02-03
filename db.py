326891dfa084ff22717c362c809a0ad11bc0bbdc
rrt270899 <rajnishranjantiwary12@gmail.com>
2025-02-03 12:47:00 +0000

    Create db.py

---
+++ db.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def setup_database():
+    conn = sqlite3.connect("loans.db")
+    cursor = conn.cursor()
+    
+    cursor.execute(
+    CREATE TABLE LoanRecords (
+        amount INTEGER,
+        interest INTEGER,  
+        tenure INTEGER,
+        emi INTEGER
+    ))
+    
+    conn.commit()
+    conn.close()
+
+def insert_loan(amount, interest, tenure, emi):
+    conn = sqlite3.connect("loans.db")
+    cursor = conn.cursor()
+    
+    cursor.execute("INSERT INTO LoanRecords (amount, interest, tenure, emi) VALUES (?, ?, ?, ?)", 
+                   (amount, interest, tenure, emi))
+    
+    conn.commit()
+    conn.close()
