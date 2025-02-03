7b2f6078da683efcd4925ab63770f29434f74100
rrt270899 <rajnishranjantiwary12@gmail.com>
2025-02-03 12:48:14 +0000

    Create frontend.py

---
+++ frontend.py
@@ -0,0 +1,15 @@
+document.getElementById("calculate").addEventListener("click", function() {
+    let amount = document.getElementById("amount").value;
+    let interest = document.getElementById("interest").value;
+    let tenure = document.getElementById("tenure").value;
+
+    fetch("http://127.0.0.1:5000/calculate_emi", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ amount: amount, interest: interest, tenure: tenure })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("result").innerText = "EMI: " + data.emi;
+    });
+});
